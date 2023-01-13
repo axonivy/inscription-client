d08a91d49b52837f5e37dc2d7054670659316fe4
Lukas Lieb <lukas.lieb@axonivy.com>
2023-01-13 14:41:26 +0100

    Remove server package (integrated in core)

--- server/common/com.axonivy.nextgen.common/src/main/java/com/axonivy/nextgen/common/launch/ServerLauncher.java
+++
@@ -1,9 +0,0 @@
-package com.axonivy.nextgen.common.launch;
-
-import com.axonivy.nextgen.common.IDisposable;
-
-public interface ServerLauncher extends IDisposable {
-	String getName();
-
-	void start(final String hostname, final int port);
-}
