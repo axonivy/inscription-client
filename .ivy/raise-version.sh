b1765244ff3e98ed88e79d37abb578c42954132f
Christian Strebel <christian.strebel@axonivy.com>
2023-04-04 12:21:48 +0200

    XIVY-10876 Fix raise-versions.sh

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mvn --batch-mode -f integrations/standalone/pom.xml versions:set versions:commit -DnewVersion=${1}
-mvn --batch-mode -f integrations/standalone/tests/screenshots/pom.xmlversions:set versions:commit -DnewVersion=${1}
+mvn --batch-mode -f integrations/standalone/tests/screenshots/pom.xml versions:set versions:commit -DnewVersion=${1}
 mvn --batch-mode -f integrations/standalone/tests/engine/project/pom.xml versions:set versions:commit -DnewVersion=${1}
 mvn --batch-mode -f integrations/standalone/tests/screenshots/project/pom.xml versions:set versions:commit -DnewVersion=${1}
