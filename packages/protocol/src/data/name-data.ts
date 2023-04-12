9c9954bd6ec567d9d7f72b2109c5fa5a82345e10
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-04-12 15:46:05 +0200

    omit 'config' from NameData

--- packages/protocol/src/data/name-data.ts
+++ packages/protocol/src/data/name-data.ts
@@ -1,11 +1,10 @@
-import { Document, Data } from "./inscription";
+import { Document, Data } from './inscription';
 
-export interface NameData extends Data{};
+export type NameData = Omit<Data, 'config'>;
 
 export const DEFAULT_NAME_DATA: NameData = {
   name: '',
   description: '',
-  config: {},
   docs: [] as Document[],
   tags: [] as string[]
 };
