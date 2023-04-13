0c46cb3176a8c70cce39fad03e3795abdb37684b
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-04-13 09:00:11 +0200

    refactor: do not store schema-file
    - less IO

--- packages/protocol/schemaCodegen.js
+++ packages/protocol/schemaCodegen.js
@@ -4,8 +4,7 @@ const { exit } = require('process');
 const fs = require('fs');
 
 const tsOut = './src/data/inscription.ts';
-const schemaFile = 'inscription.json';
-var schema = 'https://json-schema.axonivy.com/process/11.1.22/inscription.json';
+var schemaUri = 'https://json-schema.axonivy.com/process/11.1.22/inscription.json';
 
 const args = process.argv.slice(2);
 if (args.length > 0) {
@@ -16,25 +15,27 @@ if (args.length > 0) {
     }
     exit(0);
   }
-  schema = args[0];
+  schemaUri = args[0];
 }
 
-function fetchSchema() {
+async function fetchSchema() {
   const http = require('https');
-  const file = fs.createWriteStream(schemaFile);
-  http.get(schema, function (response) {
-    response.pipe(file);
-    file.on('finish', () => {
-      file.close();
-      console.log('Downloaded ' + schema);
-      codegen();
+  http.get(schemaUri, res => {
+    res.setEncoding('utf8');
+    let body = '';
+    res.on('data', data => {
+      body += data;
+    });
+    res.on('end', () => {
+      body = JSON.parse(body);
+      codegen(body);
     });
   });
 }
 
-function codegen() {
+function codegen(schema) {
   const tsGen = require('json-schema-to-typescript');
-  tsGen.compileFromFile(schemaFile).then(ts => {
+  tsGen.compile(schema, 'inscription').then(ts => {
     const nonNullTs = ts.replace(/\?:/g, ':');
     fs.writeFileSync(tsOut, nonNullTs);
     console.log(`generated ${tsOut}`);
