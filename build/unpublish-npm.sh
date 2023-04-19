cff77fd367c4f5a6de74b23265910aeaaf133aaf
Christian Strebel <christian.strebel@axonivy.com>
2023-04-19 12:09:21 +0200

    Add an unpublish script
    ./unpublish-npm.sh versionParam
    Unpublishes the given version from the npm registry.

---
+++ build/unpublish-npm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+REGISTRY="https://npmjs-registry.ivyteam.io/"
+
+npm unpublish "@axonivy/editor-icons@${1}" --registry $REGISTRY
+npm unpublish "@axonivy/inscription-core@${1}" --registry $REGISTRY
+npm unpublish "@axonivy/inscription-editor@${1}" --registry $REGISTRY
+npm unpublish "@axonivy/inscription-protocol@${1}" --registry $REGISTRY
