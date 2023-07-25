8b8ba3d8511c1141239b23ec0fc0755266390596
Reto Weiss <reto.weiss@axonivy.com>
2023-07-25 10:02:58 +0200

    Rewrite name tab tests to use page objects

--- integrations/standalone/tests/integration/parts/name.ts
+++ integrations/standalone/tests/integration/parts/name.ts
@@ -1,9 +1,27 @@
-import { expect } from '@playwright/test';
 import { Part } from '../../pageobjects/Part';
-import { CollapseUtil } from '../../utils/collapse-util';
-import { TableUtil } from '../../utils/table-util';
-import { TagUtil } from '../../utils/tag-util';
+import { Section } from '../../pageobjects/Section';
+import { Table } from '../../pageobjects/Table';
+import { Tags } from '../../pageobjects/Tags';
+import { TextArea } from '../../pageobjects/TextArea';
 import { PartTest } from './part-tester';
+
+class Name {
+  displayName: TextArea;
+  description: TextArea;
+  meansDocumentsSection: Section;
+  documents: Table;
+  tagsSection: Section;
+  tags: Tags;
+
+  constructor(part: Part) {
+    this.displayName = part.textArea('Display name');
+    this.description = part.textArea('Description');
+    this.meansDocumentsSection = part.section('Means / Documents');
+    this.documents = this.meansDocumentsSection.table();
+    this.tagsSection = part.section('Tags');
+    this.tags = this.tagsSection.tags();
+  }
+}
 
 export class NameTester implements PartTest {
   constructor(private readonly hasTags: boolean = true) {}
@@ -11,38 +29,45 @@ export class NameTester implements PartTest {
   partName() {
     return 'Name';
   }
-  async fill({ page }: Part) {
-    await page.getByLabel('Display name').fill('test name');
-    await page.getByLabel('Description').fill('test desc');
-    await CollapseUtil.open(page, 'Means / Documents');
-    await TableUtil.addRow(page);
-    await TableUtil.fillRow(page, 0, ['test doc', 'test url']);
+  async fill(part: Part) {
+    const name = new Name(part);
+    await name.displayName.fill('test name');
+    await name.description.fill('test desc');
+    await name.meansDocumentsSection.toggle();
+    const row = await name.documents.addRow();
+    await row.fill(['test doc', 'test url']);
     if (this.hasTags) {
-      await TagUtil.addTags(page, ['abc', 'efg']);
+      await name.tagsSection.toggle();
+      await name.tags.addTags(['abc', 'efg']);
     }
   }
-  async assertFill({ page }: Part) {
-    await expect(page.getByLabel('Display name')).toHaveValue('test name');
-    await expect(page.getByLabel('Description')).toHaveValue('test desc');
-    await TableUtil.assertRow(page, 0, ['test doc', 'test url']);
+
+  async assertFill(part: Part) {
+    const name = new Name(part);
+    await name.displayName.expectValue('test name');
+    await name.description.expectValue('test desc');
+    await name.documents.expectRowCount(1);
+    await name.documents.row(0).expectValues(['test doc', 'test url']);
     if (this.hasTags) {
-      await TagUtil.assertTags(page, ['abc', 'efg']);
+      await name.tags.expectTags(['abc', 'efg']);
     }
   }
-  async clear({ page }: Part) {
-    await page.getByLabel('Display name').clear();
-    await page.getByLabel('Description').clear();
-    await TableUtil.removeRow(page, 0);
+  async clear(part: Part) {
+    const name = new Name(part);
+    await name.displayName.clear();
+    await name.description.clear();
+    await name.documents.clear();
     if (this.hasTags) {
-      await TagUtil.clearTags(page, ['abc', 'efg']);
+      await name.tags.clearTags(['abc', 'efg']);
     }
   }
-  async assertClear({ page }: Part) {
-    await expect(page.getByLabel('Display name')).toBeEmpty();
-    await expect(page.getByLabel('Description')).toBeEmpty();
-    await CollapseUtil.assertClosed(page, 'Means / Documents');
+  async assertClear(part: Part) {
+    const name = new Name(part);
+    await name.displayName.expectEmpty();
+    await name.description.expectEmpty();
+    await name.meansDocumentsSection.isClosed();
     if (this.hasTags) {
-      await TagUtil.assertEmpty(page);
+      await name.tags.expectEmpty();
     }
   }
 }
