4b8aa62b9bf44b8603e08f6a30a4a53f3b544e87
Lukas Lieb <lukas.lieb@axonivy.com>
2023-08-25 08:41:12 +0200

    XIVY-11710 Synch name of error end with error

---
+++ packages/editor/src/components/parts/error/useErrorThrowData.test.ts
@@ -0,0 +1,25 @@
+import { DeepPartial, renderHook } from 'test-utils';
+import { ElementData, ErrorThrowData } from '@axonivy/inscription-protocol';
+import { useErrorThrowData } from './useErrorThrowData';
+
+describe('useErrorThrowData', () => {
+  function renderDataHook(errorData: DeepPartial<ErrorThrowData>) {
+    let data: DeepPartial<ElementData> = { name: 'test', config: errorData };
+    const view = renderHook(() => useErrorThrowData(), { wrapperProps: { data, setData: newData => (data = newData) } });
+    return { view, data: () => data };
+  }
+
+  test('in synch', () => {
+    const { view, data } = renderDataHook({ throws: { error: 'test' } });
+
+    view.result.current.update('error', 'myCoolName');
+    expect(data().name).toEqual('myCoolName');
+  });
+
+  test('not in synch', () => {
+    const { view, data } = renderDataHook({ throws: { error: 'error' } });
+
+    view.result.current.update('error', 'myCoolName');
+    expect(data().name).toEqual('test');
+  });
+});
