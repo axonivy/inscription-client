85484473a3f9ab69b7cf078ca37d9f9435d5b312
Lukas Lieb <lukas.lieb@axonivy.com>
2023-10-24 08:25:59 +0200

    Remove static imports, seems no longer needed

--- packages/core/src/monaco-util.ts
+++ packages/core/src/monaco-util.ts
@@ -1,16 +1,3 @@
-import 'monaco-editor/esm/vs/editor/editor.all.js';
-
-// support all editor features
-import 'monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneCommandsQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneGotoSymbolQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickAccess/standaloneHelpQuickAccess.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/quickInput/standaloneQuickInputService.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/referenceSearch/standaloneReferenceSearch.js';
-import 'monaco-editor/esm/vs/editor/standalone/browser/toggleHighContrast/toggleHighContrast.js';
-
 // @ts-ignore
 // eslint-disable-next-line import/no-unresolved
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
