8a8bef7eed7864967e34ac4763abeec49e4209e3
Christian Strebel <christian.strebel@axonivy.com>
2023-11-10 16:09:24 +0100

    Add global tsc build and copy css with tscp
    - typescript-cp copies all files which are not source files and analyzes tsconfig
    - removed old copyfiles approach

--- integrations/standalone/src/index.tsx
+++ integrations/standalone/src/index.tsx
@@ -1,5 +1,5 @@
 import './index.css';
-import { IvyScriptLanguage, InscriptionClientJsonRpc , MonacoUtil } from '@axonivy/inscription-core';
+import { IvyScriptLanguage, InscriptionClientJsonRpc, MonacoUtil } from '@axonivy/inscription-core';
 import {
   App,
   AppStateView,
