2b4fd396404debc32b6b2018113df8b7ec1e94e4
Christian Strebel <christian.strebel@axonivy.com>
2023-11-13 10:09:09 +0100

    Set type module in package.json
    - Rename eslint and jest config files form .js to .cjs
    - This means they are explicitily common js files

--- packages/editor/jest.config.cjs
+++ packages/editor/jest.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/**/src/**/*.{spec,test}.{ts,tsx}'],
   transform: {
-    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/jest/babelTransform.js',
-    '^.+\\.css$': '<rootDir>/jest/emptyTransform.js',
-    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/jest/emptyTransform.js'
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/jest/babelTransform.cjs',
+    '^.+\\.css$': '<rootDir>/jest/emptyTransform.cjs',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/jest/emptyTransform.cjs'
   },
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$', '^.+\\.module\\.(css|sass|scss)$'],
   setupFilesAfterEnv: ['<rootDir>/src/test-utils/setupTests.tsx'],
@@ -22,5 +22,5 @@ module.exports = {
       }
     ]
   ],
-  resolver: '<rootDir>/jest/resolver.js'
+  resolver: '<rootDir>/jest/resolver.cjs'
 };
