c1defd7e8b6ed7fb079ed20299dd243e61cc104f
Lukas Lieb <lukas.lieb@axonivy.com>
2024-03-28 15:59:15 +0100

    XIVY-13510 Cleanup - Use ThemeProvider of ui-components - Use theme colors/variables from ui-components

--- packages/editor/src/context/index.ts
+++ packages/editor/src/context/index.ts
@@ -2,7 +2,6 @@ export * from './useClient';
 export * from './useDataContext';
 export * from './useValidation';
 export * from './useEditorContext';
-export * from './useTheme';
 export * from './useAction';
 export * from './usePath';
 export * from './useMeta';
