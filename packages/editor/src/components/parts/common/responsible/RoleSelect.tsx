06965f587d25b02f9a09736a3d669362a0fc3794
Lukas Lieb <lukas.lieb@axonivy.com>
2024-04-08 08:15:50 +0200

    XIVY-13510 Adapt UI
    - Fix styles about old no longer exiting classes (browser etc)

--- packages/editor/src/components/parts/common/responsible/RoleSelect.tsx
+++ packages/editor/src/components/parts/common/responsible/RoleSelect.tsx
@@ -1,4 +1,3 @@
-import './RoleSelect.css';
 import { useMemo } from 'react';
 import type { SelectItem } from '../../../widgets';
 import { Select } from '../../../widgets';
@@ -6,6 +5,7 @@ import { useRoles } from './useRoles';
 import { Browser, useBrowser } from '../../../browser';
 import { usePath } from '../../../../context';
 import type { BrowserValue } from '../../../browser/Browser';
+import { Flex } from '@axonivy/ui-components';
 
 const DEFAULT_ROLE: SelectItem = { label: 'Everybody', value: 'Everybody' } as const;
 
@@ -27,7 +27,7 @@ const RoleSelect = ({ value, onChange, showTaskRoles }: RoleSelectProps) => {
   }, [value, roleItems]);
 
   return (
-    <div className='role-select'>
+    <Flex direction='row' alignItems='center' gap={1} className='role-select'>
       <Select items={roleItems} value={selectedRole} onChange={item => onChange(item.value)} />
       <Browser
         {...browser}
@@ -36,7 +36,7 @@ const RoleSelect = ({ value, onChange, showTaskRoles }: RoleSelectProps) => {
         accept={(change: BrowserValue) => onChange(change.cursorValue)}
         roleOptions={{ showTaskRoles }}
       />
-    </div>
+    </Flex>
   );
 };
 
